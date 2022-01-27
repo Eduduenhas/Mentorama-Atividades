ca8cc280a9e7b796b8299d422fdda5d10d1be23c
Eduardo Gonçalves Duenhas <Eduardoduenhas05@hotmail.com>
2022-01-26 23:07:38 -0300

    Sneak Prelude Atividade

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			minify: false
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(sass|css)$/,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-		],
-	},
-}
+  entry: './src/index.js',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
